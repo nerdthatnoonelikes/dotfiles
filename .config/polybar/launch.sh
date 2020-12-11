97fba64283dccd6304d211a5eba1efe8caad3cfd
nerdthatnoonelikes <kenrag24@gmail.com>
2020-12-10 22:15:53 -0600

    update alacritty and polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,11 +1,5 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-# Terminate already running bar instances
-killall -q polybar
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch polybar
-polybar main -c $HOME/.config/polybar/config.ini &
+polybar main --config=~/.config/polybar/config.ini
